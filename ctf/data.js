48f10ee8ca72e37d223d069d5f676424046db096
s1kr10s <miguelmendez@hotmail.cl>
2025-07-19 15:48:06 -0400

    Update data.js

--- ctf/data.js
+++ ctf/data.js
@@ -1,11 +1,11 @@
 const vulnerabilities = [
   { name: "mining_hero", category: "format string" },
   { name: "formatLaundromat", category: "format string" },
-  { name: "invoice", category: "integer overflow, stack buffer overflow" },
+  { name: "invoice", category: "integer overflow ~ stack buffer overflow" },
   { name: "steghide", category: "stack buffer overflow" },
-  { name: "user_application_firewall", category: "tcache poisoning, use after free, free hook" },
-  { name: "executor_revenge", category: "seccomp, side channel" },
-  { name: "dmz_firewall", category: "seccomp, use after free" },
+  { name: "user_application_firewall", category: "tcache poisoning ~ use after free ~ free hook" },
+  { name: "executor_revenge", category: "seccomp ~ side channel" },
+  { name: "dmz_firewall", category: "seccomp ~ use after free" },
   { name: "little_boi", category: "srop" },
-  { name: "dmz_firewall", category: "setcontext, use after free, read after free" }
+  { name: "dmz_firewall", category: "setcontext ~ use after free ~ read after free" }
 ];
